2aa8cc10ecd2bcb6326716d81ceb13caf24a75c3
Remi03 <t108590003@ntut.org.tw>
2021-12-03 15:11:02 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,25 @@
 <template>
-  <Top/>
-  <router-view/>
+  <top/>
 </template>
+
 <script>
-  import Top from './components/top.vue'
-  export default {
-      name: "index",
-      components: {
-        Top
-      }
+import top from './components/top.vue'
+
+export default {
+  name: 'App',
+  components: {
+    top
   }
+}
 </script>
-<style lang="scss">
+
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
