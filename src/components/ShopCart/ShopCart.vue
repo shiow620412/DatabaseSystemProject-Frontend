4c3d3b8a26173b28c236c65052b54f4b1d2decf8
AndyLee <t108590001@ntut.org.tw>
2021-12-03 23:49:52 +0800

    fix check

--- src/components/ShopCart/ShopCart.vue
+++ src/components/ShopCart/ShopCart.vue
@@ -14,7 +14,7 @@
         <ul>
             <li class="header">
                 <div class="pro-check">
-                    <el-checkbox v-model="isAllCheck">全選</el-checkbox>
+                    <el-checkbox v-model="isAllCheck" @click="checkAll()">全選</el-checkbox>
                 </div>
                 <div class="pro-img">商品圖片</div>
                 <div class="pro-name">商品名稱</div>
@@ -26,8 +26,6 @@
 
             <li class="product-list" v-for="(item, index) in productArray" :key=index>
                 <div class="pro-check">
-                    <!-- <el-checkbox :value="item.check" @change="checkChange($event, index)"></el-checkbox> -->
-                    <!-- <el-checkbox :value="item.check"></el-checkbox> -->
                     <el-checkbox v-model="item.check"></el-checkbox>
                 </div>
                 <div class="pro-img">
@@ -37,19 +35,11 @@
 
                 <div class="pro-price">{{ item.price }}元</div>
                 <div class="pro-quantitry">
-                    <!-- <el-input-number size="small" :value="item.quantitry" :min="1" :max="item.stock"></el-input-number> -->
                     <el-input-number size="small" v-model="item.quantitry" :min="1" :max="item.stock"></el-input-number>
                 </div>
                 <div class="pro-total pro-total-in">{{ item.quantitry * item.price }}元</div>
                 <div class="pro-action">
-                        <i class="el-icon-delete" style="font-size: 18px;" @click="deleteProduct(index)"></i>
-                    <!-- <el-popover placement="right">
-                        <p>確定要刪除嗎?</p>
-                        <div style="text-align: right; margin: 10px 0 0">
-                            <el-button type="primary" size="mini" @click="deleteProduct(index)">確定</el-button>
-                        </div>
-                        <i class="el-icon-error" style="font-size: 18px;"></i>
-                    </el-popover> -->
+                    <i class="el-icon-delete" style="font-size: 18px;" @click="deleteProduct(index)"></i>
                 </div>
             </li>
         </ul>
@@ -60,7 +50,6 @@
                 <span class="cart-total">
                     共
                     <span class="cart-total-num">{{ getQuantitry() }}</span> 件商品，已選擇
-                    <!-- <span class="cart-total-num">{{ this.productArray.length }}</span> 件商品，已選擇 -->
                     <span class="cart-total-num">{{ getCheckQuantitry() }}</span> 件
                 </span>
             </div>
@@ -69,7 +58,9 @@
                     <span class="total-price-title">合計：</span>
                     <span class="total-price">{{ getTotalPrice() }}元</span>
                 </span>
+                <!-- <router-link :to="getCheckQuantitry() > 0 ? '../products' : ''"> -->
                 <div :class="getCheckQuantitry() > 0 ? 'btn-primary' : 'btn-primary-disabled'">結帳</div>
+                <!-- </router-link> -->
             </div>
         </div>
     </div>
@@ -79,7 +70,6 @@
             <p>快去購物吧！</p>
         </div>
     </div>
-
 </div>
 </template>
 
@@ -98,7 +88,6 @@ export default {
                 productName: 'T-shirt',
                 price: 300,
                 quantitry: 5,
-                total: 1500,
                 stock: 5
             }, {
                 check: false,
@@ -106,7 +95,6 @@ export default {
                 productName: 'Tableware',
                 price: 100,
                 quantitry: 3,
-                total: 300,
                 stock: 15
             }, {
                 check: false,
@@ -114,7 +102,6 @@ export default {
                 productName: 'Book',
                 price: 50,
                 quantitry: 7,
-                total: 350,
                 stock: 35
             }, {
                 check: false,
@@ -122,7 +109,6 @@ export default {
                 productName: 'Bag',
                 price: 550,
                 quantitry: 1,
-                total: 550,
                 stock: 3
             }, {
                 check: false,
@@ -130,36 +116,39 @@ export default {
                 productName: 'Pencil Box',
                 price: 135,
                 quantitry: 1,
-                total: 135,
                 stock: 2
             }]
         }
     },
     methods: {
-        // ClickDown: function (num) {
-        //     // alert("減少數量");
-        //     if (this.productArray[num].quantitry === 1) {
-        //         if (confirm("確定要將此商品從購物車中移除?")) {
-        //             this.productArray.splice(num, 1)
-        //         }
-        //     } else if (this.productArray[num].quantitry > 1) {
-        //         this.productArray[num].quantitry--
-        //     }
-        // },
-        // ClickUp: function (num, maxCt) {
-        //     // alert("增加數量")
-        //     if (this.productArray[num].quantitry < maxCt) {
-        //         this.productArray[num].quantitry++
-        //     } else {
-        //         alert('此商品的數量只剩下 ' + maxCt + ' 個!')
-        //     }
-        // },
-        deleteProduct: function (num) {
+        deleteProduct(num) {
             if (confirm("確定要將此商品從購物車中移除?")) {
-            this.productArray.splice(num, 1)
+                this.productArray.splice(num, 1)
             }
         },
-        getQuantitry: function() {
+        checkAll() {
+            if (this.isAllCheck) {
+                for (let i = 0; i < this.productArray.length; i++) {
+                    this.productArray[i].check = false;
+                }
+            } else {
+                for (let i = 0; i < this.productArray.length; i++) {
+                    this.productArray[i].check = true;
+                }
+            }
+        },
+        getIsAllCheck() {
+            let isAllCheck = true;
+            for (let i = 0; i < this.productArray.length; i++) {
+                const temp = this.productArray[i];
+                if (!temp.check) {
+                    isAllCheck = false;
+                    return isAllCheck;
+                }
+            }
+            return isAllCheck;
+        },
+        getQuantitry() {
             let totalQuantitry = 0;
             for (let i = 0; i < this.productArray.length; i++) {
                 const temp = this.productArray[i];
@@ -167,25 +156,7 @@ export default {
             }
             return totalQuantitry;
         },
-        // updateShoppingCart (state, payload) {
-        //     if (payload.prop == "quantitry") {
-        //         if (this.productArray[payload.key].maxNum < payload.val) {
-        //             return;
-        //         }
-        //         if (payload.val < 1) {
-        //             return;
-        //         }
-        //     }
-        //     this.productArray[payload.key][payload.prop] = payload.val;
-        // },
-        // checkChange(val, key) {
-        //     this.updateShoppingCart({
-        //         key: key,
-        //         prop: "check",
-        //         val: val
-        //     });
-        // },
-        getCheckQuantitry: function() {
+        getCheckQuantitry() {
             let totalQuantitry = 0
             for (let i = 0; i < this.productArray.length; i++) {
                 const temp = this.productArray[i]
@@ -195,7 +166,7 @@ export default {
             }
             return totalQuantitry
         },
-        getTotalPrice: function() {
+        getTotalPrice() {
             let totalPrice = 0;
             for (let i = 0; i < this.productArray.length; i++) {
                 const temp = this.productArray[i];
@@ -212,7 +183,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 
 <style scoped>
-@import '../../assets/css/ShoppingCart.css';
+@import './ShopCart.scss';
 
 h3 {
     margin: 40px 0 0;
