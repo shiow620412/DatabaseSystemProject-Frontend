7c10653fea990af70a45bda102ad8d92e7094dac
AndyLee <t108590001@ntut.org.tw>
2021-12-06 21:34:04 +0800

    Organize unused code and delete

--- src/components/ShopCart/ShopCart.vue
+++ src/components/ShopCart/ShopCart.vue
@@ -12,31 +12,33 @@
 
     <div class="content" v-if="productArray.length > 0">
         <ul>
-
-            <el-table :data="productArray" style="width: 100%;" @selection-change="test">
-                <el-table-column type="selection" label="選取" min-width="10%"></el-table-column>
-                <el-table-column label="商品圖片" min-width="30%">
+            <el-table :data="productArray" style="width: 100%;font-size: 15px" @selection-change="addToPayArray">
+                <el-table-column type="selection" label="選取" min-width="10%" align="center"></el-table-column>
+                <el-table-column label="商品圖片" min-width="30%" align="center">
                     <template v-slot="scope">
                         <el-image :src="scope.row.photo"></el-image>
                     </template>
                 </el-table-column>
-                <el-table-column prop="productName" label="商品名稱" min-width="35%"></el-table-column>
-                <el-table-column prop="price" label="單價" min-width="15%"></el-table-column>
-                <el-table-column label="購買數量" min-width="35%">
+                <el-table-column prop="productName" label="商品名稱" min-width="35%" align="center"></el-table-column>
+                <el-table-column label="單價" min-width="15%" align="center">
+                    <template v-slot="scope">
+                        <el-text>&#36; {{ scope.row.price }}</el-text>
+                    </template>
+                </el-table-column>
+                <el-table-column label="購買數量" min-width="35%" align="center">
                     <template v-slot="scope">
                         <el-input-number size="small" v-model="scope.row.quantity" :min="1" :max="scope.row.stock"></el-input-number>
                     </template>
                 </el-table-column>
-                <el-table-column label="小計" min-width="20%">
+                <el-table-column label="小計" min-width="20%" align="center">
                     <template v-slot="scope">
-                        <el-text>{{ scope.row.price * scope.row.quantity }}</el-text>
+                        <el-text>&#36; {{ scope.row.price * scope.row.quantity }}</el-text>
                     </template>
                 </el-table-column>
-                <el-table-column label="操作" min-width="15%">
-
+                <el-table-column label="操作" min-width="15%" align="center">
                     <template v-slot="scope">
                         <!-- <el-button type="text" class="el-icon-delete" style="font-size: 18px;" @click="dialogVisible = true"></el-button> -->
-                        <el-button type="text" class="el-icon-delete" style="font-size: 18px;" @click="getIndex(scope.$index) & (dialogVisible = true)"></el-button>
+                        <el-button type="text" class="el-icon-delete" style="font-size: 18px;" @click="setIndex(scope.$index) & (dialogVisible = true)"></el-button>
                         <el-dialog v-model="dialogVisible" width="30%">
                             <span>您確定要將此商品從購物車中移除嗎?</span>
                             <template #footer>
@@ -47,49 +49,8 @@
                             </template>
                         </el-dialog>
                     </template>
-
                 </el-table-column>
             </el-table>
-
-            <!-- <li class="header">
-                <div class="pro-check">
-                    <el-checkbox v-model="isAllCheck" @click="checkAll()">全選</el-checkbox>
-                </div>
-                <div class="pro-img">商品圖片</div>
-                <div class="pro-name">商品名稱</div>
-                <div class="pro-price">單價</div>
-                <div class="pro-quantitry">購買數量</div>
-                <div class="pro-total">小計</div>
-                <div class="pro-action">操作</div>
-            </li>
-
-            <li class="product-list" v-for="(item, index) in productArray" :key=index>
-                <div class="pro-check">
-                    <el-checkbox v-model="item.check" @change="allCheck()"></el-checkbox>
-                </div>
-                <div class="pro-img">
-                    <img class="rounded" width="80" height="55" :src="item.photo">
-                </div>
-                <div class="pro-name">{{ item.productName }}</div>
-
-                <div class="pro-price">{{ item.price }}元</div>
-                <div class="pro-quantitry">
-                    <el-input-number size="small" v-model="item.quantity" :min="1" :max="item.stock"></el-input-number>
-                </div>
-                <div class="pro-total pro-total-in">{{ item.quantity * item.price }}元</div>
-                <div class="pro-action">
-                    <el-button type="text" class="el-icon-delete" style="font-size: 18px;" @click="dialogVisible = true"></el-button>
-                    <el-dialog v-model="dialogVisible" width="30%" show-close=false>
-                        <span>您確定要將此商品從購物車中移除嗎?</span>
-                        <template #footer>
-                            <span class="dialog-footer">
-                                <el-button @click="dialogVisible = false">取消</el-button>
-                                <el-button type="primary" @click="deleteProduct(index)">確定</el-button>
-                            </span>
-                        </template>
-                    </el-dialog>
-                </div>
-            </li> -->
         </ul>
 
         <div style="height: 20px; background-color: #f5f5f5"></div>
@@ -107,7 +68,7 @@
                     <span class="total-price">{{ getTotalPrice }}元</span>
                 </span>
                 <!-- <router-link :to="getCheckQuantity() > 0 ? '../products' : ''"> -->
-                <div :class="getCheckQuantity > 0 ? 'btn-primary' : 'btn-primary-disabled'"><button @click="clet">結帳</button></div>
+                <div :class="getCheckQuantity > 0 ? 'btn-primary' : 'btn-primary-disabled'">結帳</div>
                 <!-- </router-link> -->
             </div>
         </div>
@@ -138,7 +99,8 @@ export default {
     },
     data() {
         return {
-            isAllCheck: false,
+            productOfChecked: [],
+            deleteIndex: 0,
             productArray: [{
                 productID: 1,
                 photo: require('../../assets/logo.png'),
@@ -175,60 +137,19 @@ export default {
                 quantity: 1,
                 stock: 2
             }],
-            productOfChecked: [],
-            deleteIndex: 0,
         }
     },
     methods: {
-        getIndex(num) {
-            console.log(num);
+        setIndex(num) {
             this.deleteIndex = num;
         },
         deleteProduct() {
-            let num = this.deleteIndex;
-            console.log(num)
-            this.productArray.splice(num, 1)
-            this.dialogVisible = false
+            this.productArray.splice(this.deleteIndex, 1);
+            this.dialogVisible = false;
         },
-        checkAll() {
-            if (this.isAllCheck) {
-                for (let i = 0; i < this.productArray.length; i++) {
-                    this.productArray[i].check = false;
-                }
-            } else {
-                for (let i = 0; i < this.productArray.length; i++) {
-                    this.productArray[i].check = true;
-                }
-            }
+        addToPayArray(val) {
+            this.productOfChecked = val;
         },
-        allCheck() {
-            let tempNum = 0;
-            for (let i = 0; i < this.productArray.length; i++) {
-                if (this.productArray[i].check) {
-                    tempNum++;
-                }
-            }
-            if (tempNum === this.productArray.length) {
-                this.isAllCheck = true;
-            } else {
-                this.isAllCheck = false;
-            }
-        },
-        test(val) {
-            this.productOfChecked = val
-            // for (let i = 0; i < this.productArray.length; i++) {
-            //     for (let j = 0; j < val.length; j++) {
-            //         if (val[j].productID === this.productArray[i].productID) {
-            //             this.productArray[i].check = !this.productArray[i].check;
-            //         }
-            //     }
-            // }
-            console.log(this.productOfChecked)
-        },
-        clet() {
-            console.log(this.productOfChecked)
-            console.log(this.productArray)
-        }
     },
     computed: {
         getQuantity() {
@@ -249,11 +170,9 @@ export default {
         },
         getTotalPrice() {
             let totalPrice = 0;
-            for (let i = 0; i < this.productArray.length; i++) {
-                const temp = this.productArray[i];
-                if (temp.check) {
+            for (let i = 0; i < this.productOfChecked.length; i++) {
+                const temp = this.productOfChecked[i];
                     totalPrice += temp.price * temp.quantity;
-                }
             }
             return totalPrice;
         }
