e2c1078dcaab05a25ee080ac17719166b0782222
AndyLee <t108590001@ntut.org.tw>
2021-12-09 10:15:14 +0800

    add order

---
+++ src/components/management/order/order.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <div>
+        <div>
+            <el-row class="header" justify="center">
+                <div margin-top="20px">
+                    <el-text style="font-size: 25px;font-weight: bold">訂單管理</el-text>
+                </div>
+            </el-row>
+
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'order',
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss" src="./order.scss"></style>
