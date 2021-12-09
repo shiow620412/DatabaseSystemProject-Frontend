d91e6a654c81e4f7e4796d8b7bde0b6106b76531
AndyLee <t108590001@ntut.org.tw>
2021-12-09 10:15:32 +0800

    add product

---
+++ src/components/management/product/product.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <div>
+        <div>
+            <el-row class="header" justify="center">
+                <div margin-top="20px">
+                    <el-text style="font-size: 25px;font-weight: bold">商品管理</el-text>
+                </div>
+            </el-row>
+
+
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'product',
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss" src="./product.scss"></style>
