e5d3bb24d8144ba9b1d72ae0d29e3a3df44890e5
AndyLee <t108590001@ntut.org.tw>
2021-12-10 15:03:03 +0800

    manageMent: product

--- src/components/management/product/product.vue
+++ src/components/management/product/product.vue
@@ -2,12 +2,42 @@
 <div>
     <div>
         <div>
-            <el-row class="header" justify="center">
+            <el-row class="header" justify="left">
                 <div margin-top="20px">
                     <el-text style="font-size: 25px;font-weight: bold">商品管理</el-text>
                 </div>
             </el-row>
 
+            <el-tabs type="card" style="margin-bottom: 45px;">
+                <el-tab-pane>
+                    <template #label>
+                        <span class="el-icon-sold-out">
+                            修改商品
+                        </span>
+                    </template>
+                    修改商品(可以刪除商品跟改變商品狀態)
+                    #需要加上換頁面或是無窮無窮滾動</el-tab-pane>
+                <el-tab-pane>
+                    <template #label>
+                        <span class="el-icon-sell">
+                            新增商品
+                        </span>
+                    </template>
+                    新增商品(上架商品)
+                    #需要加上換頁面或是無窮無窮滾動</el-tab-pane>
+            </el-tabs>
+
+            <!-- <el-tabs :tab-position="tabPosition" style="height: 200px">
+                <el-tab-pane label="soldOut">修改</el-tab-pane>
+                <el-tab-pane label="sell">新增</el-tab-pane>
+            </el-tabs> -->
+
+            <!-- <el-tabs type="border-card">
+                <el-tab-pane label="User">User</el-tab-pane>
+                <el-tab-pane label="Config">Config</el-tab-pane>
+                <el-tab-pane label="Role">Role</el-tab-pane>
+                <el-tab-pane label="Task">Task</el-tab-pane>
+            </el-tabs> -->
 
         </div>
     </div>
@@ -19,6 +49,7 @@ export default {
     name: 'product',
     data() {
         return {
+            tabPosition: 'soldOut',
         }
     }
 }
