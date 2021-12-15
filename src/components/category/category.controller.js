9c271d48dc9289cdb4d194c05e25f0e67142180b
twm0989256926 <t108590031@ntut.org.tw>
2021-12-16 02:22:38 +0800

    connect product api

--- src/components/category/category.controller.js
+++ src/components/category/category.controller.js
@@ -11,6 +11,7 @@ export default {
     OndrinkSelected
 }
 
+// 處理字串過長問題
 function ResolveOverlongString(input){
     input.filterResult.forEach(element => {
         if(element.ProductName.length>12){
@@ -22,6 +23,7 @@ function ResolveOverlongString(input){
     this.inputTable = input.filterResult;
 }
 
+// 搜尋所有商品
 function OnSelectProduct(order){
     switch(order){
         case 0:
@@ -47,36 +49,42 @@ function OnSelectProduct(order){
     }
 }
 
+// 搜尋水果商品
 function OnfruitSelected(){
     productService.getProductsByfruit().then(data => {
         this.ResolveOverlongString(data);
     })
 }
 
+// 搜尋食品商品
 function OnfoodSelected(){
     productService.getProductsByfood().then(data => {
         this.ResolveOverlongString(data);
     })
 }
 
+// 搜尋電腦商品
 function OncomputerSelected(){
     productService.getProductsBycomputer().then(data => {
         this.ResolveOverlongString(data);
     })
 }
 
+// 搜尋衣服商品
 function OnclotheSelected(){
     productService.getProductsByclothe().then(data => {
         this.ResolveOverlongString(data);
     })
 }
 
+// 搜尋日常用品商品
 function OnSuppliesSelected(){
     productService.getProductsBySupplies().then(data => {
         this.ResolveOverlongString(data);
     })
 }
 
+// 搜尋飲料商品
 function OndrinkSelected(){
     productService.getProductsBydrink().then(data => {
         this.ResolveOverlongString(data);
